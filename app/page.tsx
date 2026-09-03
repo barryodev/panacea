449cf03a0d217610fd109c22f8ff379e628ad3cb
barryodev <barryodev@gmail.com>
2026-09-03 11:47:57 +0100

    added responsive design and changed button layout on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import WordCloud from "@/components/WordCloud";
 
 export default function Home() {
   return (
-    <main style={{ position: "relative", width: "100vw", height: "100vh" }}>
+    <main className="page-shell">
       <WordCloud />
     </main>
   );
